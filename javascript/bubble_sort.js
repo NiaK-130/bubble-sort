aef9526615a614114f2fbedf21ac4974955bc49f
Nia K <nsjkappen@gmail.com>
2022-02-02 11:00:09 -0800

    commit

--- javascript/bubble_sort.js
+++ javascript/bubble_sort.js
@@ -1,5 +1,14 @@
 function bubbleSort(arr) {
-  // type your code here
+
+  for (let i = 0; i < arr.length -1; i++){
+    for (let j = 0; j < arr.length -1; j++){
+      if (arr[j] > arr[j+1]){
+        [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+      }
+    }
+  }
+  return arr
+  
 }
 
 if (require.main === module) {
